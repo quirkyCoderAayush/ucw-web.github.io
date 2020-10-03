ce8d39f4ce75abdf1abdfafddb267f5fccd8865f
uncertain001 <50369625+uncertain001@users.noreply.github.com>
2020-10-03 18:04:44 +0530

    Refresed

--- index.js
+++ index.js
@@ -11,15 +11,3 @@ navBtn.addEventListener("click", function(){
         isRed = true;    
     }
 });
-var video = document.getElementById("myVideo");
-var btn = document.getElementById("myBtn");
-
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
